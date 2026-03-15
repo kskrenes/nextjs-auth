c9cc22598f1d242c283557e83ec334fb4ae476a3
kskrenes@gmail.com <kskrenes@gmail.com>
2026-03-15 12:25:50 -0600

    wire logout functionality on profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,36 @@
+"use client";
+
+import Button from "@/components/nae-button"
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+
 const ProfilePage = () => {
+
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      router.push("/login");
+    } catch (error: any) {
+      const message = error.response?.data?.error || error.message || "Logout failed";
+      toast.error(message);
+    }
+  }
+
   return (
-    <div>ProfilePage</div>
+    <div className="flex justify-center min-h-screen">
+      <div className="flex flex-col items-center justify-center w-[300px] py-2" >
+        <h1 className="mb-6 text-3xl font-bold">Profile Page</h1>
+        <Button 
+          className="px-8 my-8"
+          onClick={handleLogout}
+        >
+          Sign Out
+        </Button>
+      </div>
+    </div>
   )
 }
 
