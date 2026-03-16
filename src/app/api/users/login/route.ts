402c9fd2a7119faf803483da56267091f25ac6ad
kskrenes@gmail.com <kskrenes@gmail.com>
2026-03-15 18:47:03 -0600

    only import the response type

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,5 +1,5 @@
 import { connect } from "@/dbconfig/dbconfig";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 import User from "@/models/user-model";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
