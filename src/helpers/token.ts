bc85290749ed73505f15107f70cc85d06cf06a4d
kskrenes@gmail.com <kskrenes@gmail.com>
2026-03-15 18:45:46 -0600

    add helper to get user id from auth token

--- src/helpers/token.ts
+++ src/helpers/token.ts
@@ -1,1 +1,16 @@
+import jwt from "jsonwebtoken";
+import type { NextRequest } from "next/server";
+
 export const TOKEN_COOKIE_NAME = "naetoken" as const;
+
+export const getIdFromToken = async (request: NextRequest) => {
+  try {
+    const token = request.cookies.get(TOKEN_COOKIE_NAME)?.value || "";
+    // TODO: fix this typing to be more robust and include the id property
+    const decodedToken: any = jwt.verify(token, process.env.JWT_SECRET!);
+    return decodedToken.id;
+  } catch (error: any) {
+    // TODO: type error appropriately or use type guards
+    throw new Error(error.message);
+  }
+}
