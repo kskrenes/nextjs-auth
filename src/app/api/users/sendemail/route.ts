018b6868a09558ce8d59b7bd8684cb7d65a2fb2c
kskrenes@gmail.com <kskrenes@gmail.com>
2026-03-18 08:33:34 -0600

    create sendemail api for calls from client

---
+++ src/app/api/users/sendemail/route.ts
@@ -0,0 +1,83 @@
+import { sendEmail } from "@/helpers/mailer";
+import { getRequestBody } from "@/helpers/validate-request";
+import User from "@/models/user-interface";
+import { NextResponse, type NextRequest } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    // throw if request json is invalid
+    let reqBody: any;
+    try {
+      reqBody = await getRequestBody(request);
+    } catch(error: any) {
+      return NextResponse.json(
+        { error: error.message }, 
+        { status: 400 }
+      );
+    }
+
+    // throw if user is invalid
+    const user = reqBody.user as User;
+    if (!user || !user.email || !user._id) {
+      return NextResponse.json(
+        { error: "Invalid user" }, 
+        { status: 400 }
+      );
+    }
+
+    // throw if type is invalid
+    const type = reqBody.type as string;
+    if (type !== "VERIFY" && type !== "RESET") {
+      return NextResponse.json(
+        { error: "Invalid email type" }, 
+        { status: 400 }
+      );
+    }
+    
+    // attempt to send email
+    let mailResponse;
+    try {
+      mailResponse = await sendEmail({ 
+        email: user.email, 
+        emailType: type, 
+        userId: user._id, 
+      });
+    } catch (error: unknown) {
+      const eMessage = error instanceof Error ? error.message : "Error sending email";
+      return NextResponse.json(
+        { error: eMessage }, 
+        { status: 400 }
+      );
+    }
+
+    // throw if no response
+    if (!mailResponse) {
+      return NextResponse.json(
+        { error: "No response received" }, 
+        { status: 400 }
+      );
+    }
+
+    // throw if no success status
+    if (!mailResponse.response.includes("250")) {
+      return NextResponse.json(
+        { error: "Unsuccessful response:" + mailResponse.response }, 
+        { status: 400 }
+      );
+    }
+    
+    // return success and include the mail response object
+    return NextResponse.json({
+      message: "Email sent successfully",
+      success: true,
+      mailResponse,
+    })
+  }
+  catch (error: unknown) {
+    console.error("Error sending email:", error);
+    return NextResponse.json(
+      { error: "Error sending email" }, 
+      { status: 500 }
+    );
+  }
+}
