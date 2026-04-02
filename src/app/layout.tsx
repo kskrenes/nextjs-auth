39b811ad62f765593ccc4bb71cd97ba4d8bebc80
kskrenes@gmail.com <kskrenes@gmail.com>
2026-04-02 13:15:27 -0600

    wrap app content with context provider for managing authentication state in any client component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import Header from "@/components/header";
+import { AuthProvider } from "../context/AuthContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +30,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
-        <main>{children}</main>
-        <Toaster />
+        <AuthProvider>
+          <>
+            <Header />
+            <main>{children}</main>
+            <Toaster />
+          </>
+        </AuthProvider>
       </body>
     </html>
   );
