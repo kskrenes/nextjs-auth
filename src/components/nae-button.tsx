ec455a8d4bd40ea71939f0e3fb150acb2c2adb95
kskrenes@gmail.com <kskrenes@gmail.com>
2026-04-04 16:25:51 -0600

    clean up nae-button component and add twMerge so that anything passed in classname will override conflicting styles

--- src/components/nae-button.tsx
+++ src/components/nae-button.tsx
@@ -1,4 +1,5 @@
 import React, { ButtonHTMLAttributes, ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 /*
   Define the props interface by extending standard HTML button attributes.
@@ -18,16 +19,16 @@ const Button: React.FC<ButtonProps> = ({
   className = '',
   ...rest // Collect any other standard HTML button props
 }) => {
-  const baseStyles = 'px-6 py-2.5 text-lg font-semibold text-white rounded-lg transition-colors cursor-pointer flex justify-center items-center';
+  const baseStyles = 'px-6 py-2.5 text-lg font-semibold rounded-lg transition-colors cursor-pointer flex justify-center items-center';
   
   const variantStyles = variant === 'primary'
-    ? 'hover:bg-blue-700 bg-blue-600 disabled:bg-blue-900/50 disabled:text-gray-500/50 disabled:cursor-not-allowed'
-    : 'bg-gray-200 text-gray-800 hover:bg-gray-300';
+    ? 'bg-blue-600 hover:bg-blue-700 disabled:bg-blue-900/50 text-white disabled:text-gray-500/50 disabled:cursor-not-allowed'
+    : 'bg-gray-200 disabled:bg-gray-600/50 text-gray-700 disabled:text-gray-700 hover:text-blue-600 disabled:cursor-not-allowed';
 
   return (
     <button
       onClick={onClick}
-      className={`${baseStyles} ${variantStyles} ${className}`}
+      className={twMerge(baseStyles, variantStyles, className)}
       {...rest} // Spread the rest of the HTML button attributes
     >
       {children}
