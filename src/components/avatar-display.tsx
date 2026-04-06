8060866c22a6c56fa0bf26ebb3c5476732a6bb16
kskrenes@gmail.com <kskrenes@gmail.com>
2026-04-06 14:56:29 -0600

    add error handling to avatar for cases where the image fails to load

--- src/components/avatar-display.tsx
+++ src/components/avatar-display.tsx
@@ -1,6 +1,19 @@
+import { User } from 'lucide-react';
 import { CldImage } from 'next-cloudinary';
+import { useState } from 'react';
 
 export default function AvatarDisplay({ publicId }: { publicId: string }) {
+
+  const [hasError, setHasError] = useState<boolean>(false);
+
+  if (hasError) {
+    return (
+      <div className="w-52 h-52 rounded-full bg-slate-700 flex items-center justify-center">
+        <User className='w-2/3 h-2/3 text-slate-500' />
+      </div>
+    );
+  }
+
   return (
     <CldImage
       width="208"
@@ -11,6 +24,7 @@ export default function AvatarDisplay({ publicId }: { publicId: string }) {
       crop="thumb"      // Automatically crops to the most interesting part
       gravity="face"    // Ensures the face is centered in the avatar
       className="rounded-full"
+      onError={() => setHasError(true)}
     />
   );
 }
