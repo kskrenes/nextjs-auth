1c3b0f6acd5aedca9952884415eb257fb5a1ca6f
kskrenes@gmail.com <kskrenes@gmail.com>
2026-04-08 11:14:53 -0600

    update button and input styles

--- src/components/nae-input.tsx
+++ src/components/nae-input.tsx
@@ -6,7 +6,7 @@ import React, { InputHTMLAttributes } from 'react';
   explicit declaration, while also allowing custom props.
 */
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  label: string;
+  label?: string;
   instruction?: string;
   id: string; // Ensure the input has an ID for accessibility (linking label and input)
   autoComplete?: string;
@@ -21,18 +21,21 @@ const Input: React.FC<InputProps> = ({
   className = '',
   ...rest 
 }) => {
-  const baseStyles = 'p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-blue-400';
+  const baseStyles = 'p-2 border border-gray-700 text-gray-400 rounded-lg focus:outline-none focus:border-indigo-400';
   const autoCompleteValue = autoComplete || id;
   return (
-    <div className="flex flex-col space-y-2 min-w-[300px]">
-      <div className='flex justify-between items-center'>
-        {/* Use the 'htmlFor' attribute to link the label to the input via their IDs */}
-        <label className='text-sm' htmlFor={id}>{label}</label>
-        {/* Show instruction text top right */}
-        {instruction && <p className='text-xs text-gray-500'>{instruction}</p>}
-      </div>
-      
-      {/* Spread the rest of the props onto the native input element */}
+    <div className="flex flex-col gap-2 min-w-[300px] w-full">
+
+      {(label || instruction) && (
+        <div className='flex justify-between items-center'>
+          {/* show label top left */}
+          {label && <label className='text-lg font-semibold' htmlFor={id}>{label}</label>}
+          
+          {/* show instruction text top right */}
+          {instruction && <p className='text-xs text-gray-400'>{instruction}</p>}
+        </div>
+      )}
+      {/* spread the rest of the props onto the native input element */}
       <input id={id} name={id} autoComplete={autoCompleteValue} className={`${baseStyles} ${className}`} {...rest} />
     </div>
   );
