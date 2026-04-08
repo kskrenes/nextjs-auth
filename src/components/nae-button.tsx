55fe4c3714c57e65f0f6b1efe7e3b19aad991762
kskrenes@gmail.com <kskrenes@gmail.com>
2026-04-08 14:45:48 -0600

    fix disabled secondary button styles

--- src/components/nae-button.tsx
+++ src/components/nae-button.tsx
@@ -12,6 +12,22 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     'primary' | 'secondary';
 }
 
+const baseStyles = `
+  flex justify-center items-center 
+  px-6 py-2.5 rounded-md 
+  text-lg font-semibold 
+  transition-colors 
+  cursor-pointer disabled:cursor-not-allowed`;
+
+const primaryStyles = `
+  bg-indigo-500 hover:bg-indigo-600 disabled:bg-indigo-900/50 
+  text-[#e5e6f2] disabled:text-gray-500/50`;
+  
+const secondaryStyles = `
+  bg-indigo-500/0 hover:bg-indigo-600 disabled:bg-indigo-900/0
+  border-2 border-indigo-500 hover:border-indigo-600 disabled:border-indigo-900/40
+  text-[#e5e6f2] hover:text-white disabled:text-gray-700`;
+
 const Button: React.FC<ButtonProps> = ({
   children, 
   variant = 'primary', 
@@ -19,11 +35,10 @@ const Button: React.FC<ButtonProps> = ({
   className = '',
   ...rest // Collect any other standard HTML button props
 }) => {
-  const baseStyles = 'px-6 py-2.5 text-lg font-semibold rounded-md transition-colors cursor-pointer flex justify-center items-center';
   
   const variantStyles = variant === 'primary'
-    ? 'bg-indigo-500 hover:bg-indigo-600 disabled:bg-indigo-900/50 text-[#e5e6f2] disabled:text-gray-500/50 disabled:cursor-not-allowed'
-    : 'bg-indigo-500/0 hover:bg-indigo-600 disabled:bg-gray-600/50 border-2 border-indigo-500 hover:border-indigo-600 text-[#e5e6f2] disabled:text-gray-700 hover:text-white disabled:cursor-not-allowed';
+    ? primaryStyles
+    : secondaryStyles;
 
   return (
     <button
