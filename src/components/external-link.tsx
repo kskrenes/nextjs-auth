69a33a68d97c9806cc2e41d13e408148b3537c0a
kskrenes@gmail.com <kskrenes@gmail.com>
2026-04-08 12:25:41 -0600

    spread ...rest before classname to avoid overriding default styles

--- src/components/external-link.tsx
+++ src/components/external-link.tsx
@@ -6,8 +6,8 @@ const ExternalLink: React.FC<AnchorHTMLAttributes<HTMLAnchorElement>> = ({ href,
       href={href} 
       target="_blank" 
       rel="noopener noreferrer"
-      className="hover:underline hover:text-indigo-400 text-gray-400" 
       {...rest}
+      className="hover:underline hover:text-indigo-400 text-gray-400" 
     >
       {children}
     </a>
