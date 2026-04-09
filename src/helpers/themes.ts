cfe1e50a45fb712387c0d0830a8bc498a4b2cd34
kskrenes@gmail.com <kskrenes@gmail.com>
2026-04-09 11:36:08 -0600

    remove old theme code

--- src/helpers/themes.ts
+++ src/helpers/themes.ts
@@ -1,19 +1,1 @@
-export const themes = {
-  light: {
-    background: "#e5e6f2",
-    panel: '#ffffff',
-    text: "#353642",
-    secondaryButtonText: "#2f5dfc",
-  },
-  dark: {
-    background: "#252632",
-    panel: '#282a36',
-    text: "#e5e6f2",
-    secondaryButtonText: "#e5e6f2",
-  },
-};
-
-export const lightTheme = themes.light;
-export const darkTheme = themes.dark;
-export const defaultTheme = darkTheme;
 export const defaultAvatarId = 'default_potato';
