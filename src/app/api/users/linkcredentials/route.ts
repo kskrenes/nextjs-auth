e908563c69700a249e1e8128d878662fbb93e09b
kskrenes@gmail.com <kskrenes@gmail.com>
2026-04-19 08:35:25 -0600

    update deprecated 'new' mongoose field to returnDocument: 'after'

--- src/app/api/users/linkcredentials/route.ts
+++ src/app/api/users/linkcredentials/route.ts
@@ -104,7 +104,7 @@ export async function POST(request: NextRequest) {
           hasCompletedProfile: true,
         },
       },
-      { new: true }
+      { returnDocument: 'after' }
     );
 
     if (!updatedUser) {
