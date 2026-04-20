511129d8c5e3749a5630e6a2a533eca5790adfee
kskrenes@gmail.com <kskrenes@gmail.com>
2026-04-20 08:58:24 -0600

    move username and password validation into the validation helper

--- src/helpers/expression-validation.ts
+++ src/helpers/expression-validation.ts
@@ -12,3 +12,41 @@ export const excludesSpaces = (value: string): boolean => {
 export const meetsMinimum = (value: string, min: number): boolean => {
   return value.length >= min;
 }
+
+export const validateUsername = (username: string): string => {
+  const normalizedUsername = username.trim();
+
+  // enforce no whitespace
+  if (!excludesSpaces(normalizedUsername)) {
+    throw new Error("Username cannot contain spaces");
+  }
+
+  // enforce minimum length
+  if (normalizedUsername.length < 4) {
+    throw new Error("Username must meet minimum character requirement");
+  }
+
+  return normalizedUsername;
+}
+
+export const validatePassword = (password: string, confirmPassword: string): string => {
+  const normalizedPassword = password.trim();
+  const normalizedConfirm = confirmPassword.trim();
+
+  // enforce password confirmation match
+  if (normalizedPassword !== normalizedConfirm) {
+    throw new Error("Passwords do not match");
+  }
+
+  // enforce minimum length
+  if (normalizedPassword.length < 8) {
+    throw new Error("Password must be at least 8 characters");
+  }
+
+  // enforece no whitespace
+  if (!excludesSpaces(normalizedPassword)) {
+    throw new Error("Password cannot contain spaces");
+  }
+
+  return normalizedPassword;
+}
