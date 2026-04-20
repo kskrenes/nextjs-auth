1a46891de10728ff4c593a7fc0339ed12045b493
kskrenes@gmail.com <kskrenes@gmail.com>
2026-04-20 08:57:47 -0600

    move display formatters into a helper

---
+++ src/helpers/display.tsx
@@ -0,0 +1,53 @@
+import { 
+  BlueSkyIcon, FacebookIcon, InstagramIcon, LinkedInIcon, LinkIcon, 
+  MastodonIcon, RedditIcon, TwitchIcon, TwitterIcon, YouTubeIcon 
+} from "@/components/profile-icons";
+
+const socialSubstrings = ["linkedin", "facebook", "twitter", "x.com", "instagram", "youtube", "reddit", "twitch", "mastodon", "bsky"];
+const socialIconsMap: { [key: string]: React.ReactElement } = {
+  linkedin: <LinkedInIcon />,
+  twitter: <TwitterIcon />,
+  "x.com": <TwitterIcon />,
+  facebook: <FacebookIcon />,
+  instagram: <InstagramIcon />,
+  youtube: <YouTubeIcon />,
+  reddit: <RedditIcon />,
+  twitch: <TwitchIcon />,
+  mastodon: <MastodonIcon />,
+  bsky: <BlueSkyIcon />,
+};
+
+export const getDisplayLink = (url: string) => {
+  try {
+    const urlObj = new URL(url);
+    const cleanPathname = urlObj.pathname.replace(/^\/|\/$/g, ''); // remove leading and trailing slashes
+    const hostname = urlObj.hostname.toLowerCase();
+    const isSupportedSite = socialSubstrings.some(substring => hostname.includes(substring));
+
+    if (isSupportedSite) {
+      return cleanPathname || urlObj.host;
+    }
+
+    return cleanPathname ? `${urlObj.host}/${cleanPathname}` : urlObj.host;  
+  } catch {
+    // fall through to raw string if URL is invalid
+  }
+
+  return url;
+}
+
+export const getSocialIcon = (url: string) => {
+  try {
+    const urlObj = new URL(url);
+    const hostname = urlObj.hostname.toLowerCase();
+
+    const supportedSiteMatch = socialSubstrings.find(substring => hostname.includes(substring));
+    if (supportedSiteMatch) {
+      return socialIconsMap[supportedSiteMatch];
+    }
+  } catch {
+    // fall through to default icon
+  }
+  
+  return <LinkIcon />;
+}
