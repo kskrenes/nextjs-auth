61521389f2682a200db6f255f007358bbe6296e8
kskrenes@gmail.com <kskrenes@gmail.com>
2026-04-29 12:08:01 -0600

    refactor proxy.ts to return 401 directly for protected api routes when unauthorized

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,36 +1,36 @@
-import { JWTPayload, jwtVerify } from "jose";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { TOKEN_COOKIE_NAME } from "./helpers/token";
+import { ACCESS_TOKEN_COOKIE_NAME, verifyAccessToken } from "./helpers/token";
+import { JwtPayload } from "jsonwebtoken";
 
 function requireAuth(path: string) {
-  // include all pages that require authorized users
-  return (
-    path === "/dashboard" ||
-    path === "/account" ||
-    path === "/onboarding"
-  );
+  // protect client pages that require authorized users
+  const isProtectedPage = [
+    "/dashboard",
+    "/account",
+    "/onboarding",
+  ].includes(path);
+
+  // protect all API routes that require authorized users
+  const isProtectedApi = 
+    path.startsWith('api/sign-cloudinary-params') ||
+    path.startsWith('api/users/linkcredentials') ||
+    path.startsWith('api/users/update')
+
+  return isProtectedPage || isProtectedApi;
 }
 
-function rejectAuth(path: string) {
-  // include all pages that should redirect authorized users
-  return (
-    path === "/login" ||
-    path === "/signup"
-  );
-}
-
-async function getAuthTokenPayload(request: NextRequest): Promise<JWTPayload | null> {
+async function getAuthTokenPayload(request: NextRequest): Promise<JwtPayload | null> {
   // verify token exists
-  const token = request.cookies.get(TOKEN_COOKIE_NAME)?.value;
+  const token = request.cookies.get(ACCESS_TOKEN_COOKIE_NAME)?.value;
   if (!token) {
     return null;
   }
 
   // validate token integrity
   try {
-    const { payload } = await jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET));
-    return payload as JWTPayload;
+    const payload = verifyAccessToken(request);
+    return payload as JwtPayload;
   } catch (error) {
     return null;
   }
@@ -38,33 +38,47 @@ async function getAuthTokenPayload(request: NextRequest): Promise<JWTPayload | n
 
 export async function proxy(request: NextRequest) {
   const path = request.nextUrl.pathname;
-  const authRequired = requireAuth(path);
-  const authRejected = rejectAuth(path);
+  const isApiRoute = path.startsWith('/api');
   const authTokenPayload = await getAuthTokenPayload(request);
+  const needsOnboarding = authTokenPayload?.hasCompletedProfile === false;
 
-  // redirect unauthenticated users away from protected pages
-  if (authRequired && !authTokenPayload) {
-    return NextResponse.redirect(new URL('/login', request.nextUrl));
-  }
-  
-  // redirect non-onboarded authenticated users to onboarding
-  if (authTokenPayload?.hasCompletedProfile === false && path !== '/onboarding') {
-    return NextResponse.redirect(new URL('/onboarding', request.nextUrl));
+  // intercept when path requires authentication
+  if (requireAuth(path)) {
+    // handle unauthorized user
+    if (!authTokenPayload) {
+      // return a 401 for protected API routes
+      if (isApiRoute) {
+        return new NextResponse(
+          JSON.stringify({ error: 'Unauthorized' }),
+          { status: 401, headers: { 'content-type': 'application/json' } }
+        );
+      }
+
+      // redirect for protected pages
+      return NextResponse.redirect(new URL('/login', request.nextUrl));
+    
+    // handle authorized but non-onboarded user
+    } else if (needsOnboarding && path !== '/onboarding') {
+      return NextResponse.redirect(new URL('/onboarding', request.nextUrl));
+    }
   }
 
-  // redirect authenticated users away from signup/login pages
-  if (authRejected && authTokenPayload) {
+  // redirect authenticated users away from login page
+  if (authTokenPayload && path === '/login') {
     return NextResponse.redirect(new URL('/dashboard', request.nextUrl));
   }
   
   // redirect onboarded authenticated users away from onboarding
-  if (authTokenPayload?.hasCompletedProfile === true && path === '/onboarding') {
+  if (!needsOnboarding && path === '/onboarding') {
     return NextResponse.redirect(new URL('/dashboard', request.nextUrl));
   }
+
+  return NextResponse.next();
 }
 
 export const config = {
   matcher: [
+    "/api/:path*",
     "/",
     "/account",
     "/dashboard",
