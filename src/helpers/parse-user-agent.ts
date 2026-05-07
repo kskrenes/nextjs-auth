f7b7633913f1d942a3bea41bdd0ab73187bdb785
kskrenes@gmail.com <kskrenes@gmail.com>
2026-05-07 15:51:40 -0600

    concat os version to the os name value, remove extra 'unknown' from deviceType enum, and allow undefined for userAgent type check

--- src/helpers/parse-user-agent.ts
+++ src/helpers/parse-user-agent.ts
@@ -1,16 +1,18 @@
 import { UAParser } from 'ua-parser-js';
 
 export default function parseUserAgent(
-  userAgent: string
+  userAgent: string | undefined
 ): { 
-  deviceType: "mobile" | "desktop" | "tablet" | "unknown" | "console" | "embedded" | "smarttv" | "wearable" | "xr" | "unknown", 
+  deviceType: "mobile" | "desktop" | "tablet" | "console" | "embedded" | "smarttv" | "wearable" | "xr" | "unknown", 
   os: string, 
   browser: string 
 } {
   const parser = new UAParser(userAgent);
   const deviceType = parser.getDevice().type || 'unknown';
-  const os = parser.getOS().name || 'unknown';
-  const browser = parser.getBrowser().name || 'unknown';
+  const osName = parser.getOS().name || 'Unknown OS';
+  const osVersion = parser.getOS().version;
+  const browser = parser.getBrowser().name || 'Unknown Browser';
+  const os = osName + (osVersion ? ` ${osVersion}` : '');
 
   return { deviceType, os, browser };
 }
