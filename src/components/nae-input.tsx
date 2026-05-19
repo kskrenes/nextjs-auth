70df7a561758c6434841b44ab2b53b17a16e9577
kskrenes@gmail.com <kskrenes@gmail.com>
2026-05-19 11:53:08 -0600

    refactor to use forwardRef and enable the "ref" attribute for nae-input

--- src/components/nae-input.tsx
+++ src/components/nae-input.tsx
@@ -1,4 +1,4 @@
-import React, { InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes, forwardRef } from 'react';
 
 type BaseInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'aria-label' | 'aria-labelledby'> & {
   instruction?: string;
@@ -11,33 +11,39 @@ type InputProps =
   | (BaseInputProps & { label?: undefined; ['aria-label']: string; ['aria-labelledby']?: string })
   | (BaseInputProps & { label?: undefined; ['aria-label']?: string; ['aria-labelledby']: string });
 
-// Use React.FC (Function Component) or the arrow function syntax with the InputProps type.
-const Input: React.FC<InputProps> = ({
-  label, 
-  instruction = '',
-  id,
-  autoComplete,
-  className = '',
-  ...rest 
-}) => {
-  const baseStyles = 'p-2 border border-panel-highlight text-foreground-secondary rounded-lg focus:outline-none focus:border-brand-light';
-  const autoCompleteValue = autoComplete || id;
-  return (
-    <div className="flex flex-col gap-2 w-full">
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    { 
+      label, 
+      instruction = '', 
+      id, 
+      autoComplete, 
+      className = '', 
+      ...rest 
+    }, 
+    ref
+  ) => {
+    const baseStyles = 'p-2 border border-panel-highlight text-foreground-secondary rounded-lg focus:outline-none focus:border-brand-light';
+    const autoCompleteValue = autoComplete || id;
+    
+    return (
+      <div className="flex flex-col gap-2 w-full">
 
-      {(label || instruction) && (
-        <div className='flex justify-between items-center'>
-          {/* show label top left */}
-          {label && <label className='text-lg font-semibold' htmlFor={id}>{label}</label>}
-          
-          {/* show instruction text top right */}
-          {instruction && <p className='text-xs text-foreground-secondary'>{instruction}</p>}
-        </div>
-      )}
-      {/* spread the rest of the props onto the native input element */}
-      <input id={id} name={id} autoComplete={autoCompleteValue} className={`${baseStyles} ${className}`} {...rest} />
-    </div>
-  );
-};
+        {(label || instruction) && (
+          <div className='flex justify-between items-center'>
+            {/* show label top left */}
+            {label && <label className='text-lg font-semibold' htmlFor={id}>{label}</label>}
+            
+            {/* show instruction text top right */}
+            {instruction && <p className='text-xs text-foreground-secondary'>{instruction}</p>}
+          </div>
+        )}
+        {/* spread the rest of the props onto the native input element */}
+        <input id={id} name={id} autoComplete={autoCompleteValue} className={`${baseStyles} ${className}`} ref={ref} {...rest} />
+      </div>
+    )
+  })
+
+Input.displayName = 'Input';
 
 export default Input;
