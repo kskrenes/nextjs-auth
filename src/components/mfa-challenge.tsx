9f32f8337933447021fb482e507e8cf9c0448b99
kskrenes@gmail.com <kskrenes@gmail.com>
2026-05-27 14:10:00 -0500

    refactor login challenge view to reuse MFA form and controls components

--- src/components/mfa-challenge.tsx
+++ src/components/mfa-challenge.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import { SubmitEvent, useEffect, useRef, useState } from "react";
-import Input from "./nae-input";
+import { useState } from "react";
 import { useAuth } from "@/context-providers/auth-context-provider";
-import Button from "./nae-button";
-import { Switch } from "@headlessui/react";
-import { ShieldAlert } from "lucide-react";
-import NaeLoader from "./nae-loader";
 import axios from "axios";
 import toast from "react-hot-toast";
+import MFAVerifyForm from "./mfa-verify-form";
+import MFAVerifyControls from "./mfa-verify-controls";
 
 interface MFAChallengeProps {
   onCancel: () => void;
@@ -17,36 +14,30 @@ interface MFAChallengeProps {
 const MFAChallenge = ({ onCancel }: MFAChallengeProps) => {
 
   const [code, setCode] = useState<string>('');
-  const [useBackupCode, setUseBackupCode] = useState<boolean>(false);
-  const [isInvalid, setIsInvalid] = useState<boolean>(false);
+  const [disabled, setDisabled] = useState<boolean>(true);
+  const [invalid, setInvalid] = useState<boolean>(false);
   const [awaitingRedirect, setAwaitingRedirect] = useState<boolean>(false);
 
   const { verifyingMFA, verifyMFA } = useAuth();
 
-  const codeInputRef = useRef<HTMLInputElement>(null);
-  useEffect(() => {
-    // focus the verification code input
-    if (codeInputRef.current) {
-      codeInputRef.current.focus();
-    }
-  }, []);
+  const handleFormChange = (val: string, disabled: boolean) => {
+    setCode(val);
+    setDisabled(disabled);
+    setInvalid(false);
+  }
 
-  const handleVerify = async (e: SubmitEvent<HTMLFormElement>) => {
-    // suppress native html form submit behavior
-    e.preventDefault(); 
-
+  const handleVerify = async () => {
     if (verifyingMFA) return;
 
-    setIsInvalid(false);
     try {
+      setInvalid(false);
       setAwaitingRedirect(true);
-      // TODO: normalize code
       await verifyMFA(code);
     } catch (error) {
       if (axios.isAxiosError(error) && [400, 401].includes(error.response?.status ?? 0)) {
-        setIsInvalid(true);
+        setInvalid(true);
       } else {
-        toast.error("Error validating verification code");
+        toast.error("There was an error validating the verification code");
       }
       setAwaitingRedirect(false);
     }
@@ -57,71 +48,24 @@ const MFAChallenge = ({ onCancel }: MFAChallengeProps) => {
   }
 
   return (
-    <form 
-      className="flex w-75 flex-col items-center py-2 gap-8" 
-      onSubmit={handleVerify}
-    >
-      {/* title */}
+    <div className="flex flex-col w-sm gap-4">
       <h1 className="text-3xl font-bold">Two-Step Verification</h1>
-
-      {/* error message */}
-      {isInvalid && (
-        <div role="alert" className="flex items-center gap-2 text-error">
-          <ShieldAlert className="w-6 h-6" />
-          <span>Invalid code</span>
-        </div>
-      )}
-
-      <div className="w-full">
-        <Input 
-          id="mfachallengecode" 
-          label={useBackupCode ? "Backup Code" : "Verification Code"}
-          placeholder={useBackupCode ? "A1B2C3D4" : "000000"}
-          ref={codeInputRef}
-          maxLength={useBackupCode ? 8 : 6}
-          inputMode={useBackupCode ? "text" : "numeric"}
-          autoComplete="one-time-code"
-          value={code} 
-          onChange={(e) => setCode(e.target.value)}
-          className="tracking-widest"
+      <p className="text-foreground-secondary">Enter the verification code from your authentication app to complete your sign-in.</p>
+      <MFAVerifyForm 
+        onChange={handleFormChange}
+        onVerify={handleVerify}
+        loading={verifyingMFA || awaitingRedirect}
+        invalid={invalid}
+      />
+      <div className="flex gap-2 justify-end">
+        <MFAVerifyControls 
+          onCancel={handleCancel}
+          onVerify={handleVerify}
+          loading={verifyingMFA || awaitingRedirect} 
+          disabled={disabled || verifyingMFA || awaitingRedirect}
         />
       </div>
-      <div className="flex gap-4">
-        <Button 
-          type="submit"
-          disabled={verifyingMFA || awaitingRedirect || code.length < (useBackupCode ? 8 : 6)}
-        >
-          {verifyingMFA || awaitingRedirect 
-            ? (
-              <>
-                <NaeLoader className="mx-2.5" />
-                <span className="sr-only">Signing in</span>
-              </>
-            ) 
-            : 'Verify'
-          }
-        </Button>
-        <Button 
-          type="button"
-          variant="secondary"
-          disabled={verifyingMFA || awaitingRedirect}
-          onClick={handleCancel}
-        >
-          Cancel
-        </Button>
-      </div>
-      <div className="flex gap-2">
-        <Switch
-          checked={useBackupCode}
-          onChange={setUseBackupCode}
-          className={useBackupCode ? 'switch-on' : 'switch-off'}
-        >
-          <span className="sr-only">Use backup code</span>
-          <span className={useBackupCode ? 'switch-handle-on' : 'switch-handle-off'} />
-        </Switch>
-        <p>Use a backup code</p>
-      </div>
-    </form>
+    </div>
   )
 }
 
