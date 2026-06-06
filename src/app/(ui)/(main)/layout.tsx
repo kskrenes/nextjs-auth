6a52e124bbea03b8cfefb6205ae14ba4a770511d
kskrenes@gmail.com <kskrenes@gmail.com>
2026-06-06 07:52:17 -0600

    add left margin to (main) group to account for fixed sidebar

--- src/app/(ui)/(main)/layout.tsx
+++ src/app/(ui)/(main)/layout.tsx
@@ -9,7 +9,7 @@ const MainLayout = ({
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <main className="flex-1">
+      <main className="flex-1 md:ml-64">
         {children}
       </main>
     </div>
