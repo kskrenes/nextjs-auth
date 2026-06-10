0910d5105d577d5c1630f842705584e1d657d1de
kskrenes@gmail.com <kskrenes@gmail.com>
2026-06-09 18:24:57 -0600

    create the passkey delete confirmation modal

---
+++ src/components/passkey-delete-confirm-modal.tsx
@@ -0,0 +1,47 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+} from './dialog';
+import Button from './nae-button';
+
+export interface ModalProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  onCancel: () => void;
+  onConfirm: () => void;
+}
+
+const PasskeyDeleteConfirmModal = ({ open, onOpenChange, onCancel, onConfirm }: ModalProps) => {
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent className="sm:max-w-sm">
+        <DialogHeader>
+          <DialogTitle>Remove Passkey</DialogTitle>
+          <DialogDescription>
+            This passkey will be permanently removed and you won&apos;t be able to use it to sign in.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button 
+            variant="secondary"
+            onClick={onCancel} 
+          >
+            Cancel
+          </Button>
+          <Button 
+            variant="extreme"
+            onClick={onConfirm} 
+          >
+            Remove
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default PasskeyDeleteConfirmModal
