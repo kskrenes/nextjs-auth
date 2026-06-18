df7e2b44a52beadaec7411b2b6613763a90346cd
kskrenes@gmail.com <kskrenes@gmail.com>
2026-06-18 16:16:35 -0600

    handle no data state on the account page

--- src/app/(ui)/(main)/account/page.tsx
+++ src/app/(ui)/(main)/account/page.tsx
@@ -8,6 +8,7 @@ import ProfileTab from "@/components/account/profile/profile-tab";
 import ProfilePanel from "@/components/account/profile-panel";
 import SettingsTab from "@/components/account/settings/settings-tab";
 import SecurityTab from "@/components/account/security/security-tab";
+import PanelError from "@/components/panel-error";
 
 const AccountPage = () => {
 
@@ -37,67 +38,72 @@ const AccountPage = () => {
       {/* page title */}
       <h1 className="page-title-container page-title">Account Management</h1>
 
-      {/* page layout */}
-      <div className="flex gap-8 flex-col ll:flex-row">
+      {user ? (
+        <div className="flex gap-8 flex-col ll:flex-row">
         
-        {/* first column */}
-        <div className="ll:w-90">
+          {/* first column */}
+          <div className="ll:w-90">
 
-          {/* profile card */}
-          <ProfilePanel 
-            editing={isEditing} 
-            onEditClick={handleEditClick} 
-          />
+            {/* profile card */}
+            <ProfilePanel 
+              editing={isEditing} 
+              onEditClick={handleEditClick} 
+            />
+          </div>
+
+          {/* second column */}
+          <div className="w-full ll:flex-1 mx-auto md:mx-0">
+
+            <TabGroup 
+              className="flex flex-col gap-8"
+              selectedIndex={activeTab} 
+              // Programmatic tab changes (e.g. handleEditClick) bypass onChange,
+              // so setIsEditing(false) here won't conflict with setIsEditing(true) there.
+              onChange={(index) => {
+                setActiveTab(index);
+                setIsEditing(false);
+              }}
+            >
+              <TabList className="tab-list panel">
+                <Tab className="tab-list-item">
+                  Profile
+                </Tab>
+                <Tab className="tab-list-item">
+                  Settings
+                </Tab>
+                <Tab className="tab-list-item">
+                  Security
+                </Tab>
+              </TabList>
+              <TabPanels>
+
+                {/* Profile Tab */}
+                <TabPanel>
+                  <ProfileTab 
+                    editing={isEditing} 
+                    onEditComplete={handleProfileEditComplete} 
+                  />
+                </TabPanel>
+
+                {/* Settings Tab */}
+                <TabPanel>
+                  <SettingsTab />
+                </TabPanel>
+
+                {/* Security Tab */}
+                <TabPanel>
+                  <SecurityTab />
+                </TabPanel>
+                
+              </TabPanels>
+            </TabGroup>          
+          </div>
         </div>
-
-        {/* second column */}
-        <div className="w-full ll:flex-1 mx-auto md:mx-0">
-
-          <TabGroup 
-            className="flex flex-col gap-8"
-            selectedIndex={activeTab} 
-            // Programmatic tab changes (e.g. handleEditClick) bypass onChange,
-            // so setIsEditing(false) here won't conflict with setIsEditing(true) there.
-            onChange={(index) => {
-              setActiveTab(index);
-              setIsEditing(false);
-            }}
-          >
-            <TabList className="tab-list panel">
-              <Tab className="tab-list-item">
-                Profile
-              </Tab>
-              <Tab className="tab-list-item">
-                Settings
-              </Tab>
-              <Tab className="tab-list-item">
-                Security
-              </Tab>
-            </TabList>
-            <TabPanels>
-
-              {/* Profile Tab */}
-              <TabPanel>
-                <ProfileTab 
-                  editing={isEditing} 
-                  onEditComplete={handleProfileEditComplete} 
-                />
-              </TabPanel>
-
-              {/* Settings Tab */}
-              <TabPanel>
-                <SettingsTab />
-              </TabPanel>
-
-              {/* Security Tab */}
-              <TabPanel>
-                <SecurityTab />
-              </TabPanel>
-              
-            </TabPanels>
-          </TabGroup>          
-        </div>
-      </div>
+      ) : (
+        <PanelError
+          message="Sorry, there was a problem loading data for the account page. Please try signing in again."
+        />
+      )}
     </div>
   )
 }
