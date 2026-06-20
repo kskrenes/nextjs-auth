84359a9dd129307e43a9f0a5a022ab9429069ed1
kskrenes@gmail.com <kskrenes@gmail.com>
2026-06-20 14:28:02 -0600

    add html and body tags to the global error component

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -3,7 +3,13 @@
 import ErrorScreen, { ErrorProps } from "@/components/error-screen";
 
 const GlobalError = ({ reset }: ErrorProps) => {
-  return <ErrorScreen reset={reset} isCritical={true} />
+  return (
+    <html lang="en">
+      <body>
+        <ErrorScreen reset={reset} isCritical={true} />
+      </body>
+    </html>
+  )
 }
 
 export default GlobalError;
