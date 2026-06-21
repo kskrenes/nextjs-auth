e0ee0b70cdaab25d6ce7efbb4bd5c36596b8d07b
kskrenes@gmail.com <kskrenes@gmail.com>
2026-06-20 23:17:15 -0600

    update the useTruncation reference to setRef, and remove unused import

--- src/components/dashboard/allowed-domains-list-item.tsx
+++ src/components/dashboard/allowed-domains-list-item.tsx
@@ -1,20 +1,19 @@
 import { useTruncation } from '@/hooks/use-truncation';
 import { Bold, Flex, ListItem } from '@tremor/react'
 import { CheckCircle2 } from 'lucide-react'
-import React from 'react'
 
 interface AllowedDomainsListItemProps {
   domain: string;
 }
 const AllowedDomainsListItem = ({ domain }: AllowedDomainsListItemProps) => {
-  const { elementRef, isTruncated } = useTruncation();
+  const { setRef, isTruncated } = useTruncation();
   return (
     <ListItem className="py-3 border-tremor-lat">
       <Flex>
         <div className="flex items-center gap-2">
           <div className="w-2 h-2 shrink-0 bg-foreground-excellent rounded-full" />
           <Bold 
-            ref={elementRef as React.RefObject<HTMLElement>}
+            ref={setRef}
             title={isTruncated ? `@${domain}` : undefined}
             className="text-foreground-primary font-mono text-sm break-all line-clamp-1"
           >
